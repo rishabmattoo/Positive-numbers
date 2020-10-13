0799c4b023fa8258214858e0987766afc2acdfbc
rishabmattoo <72668951+rishabmattoo@users.noreply.github.com>
2020-10-13 18:19:46 +0530

    Add files via upload

---
+++ Positive numbers.py
@@ -0,0 +1,11 @@
+#positive numbers
+list1 = [12,-7,5,64,-14]
+for i in list1:
+    if i>=0:
+        print(i,end = " ")
+       
+list2 = [12,14,-95,3]
+print("\n")
+for j in list2:
+    if j>=0:
+        print(j,end = " ")
